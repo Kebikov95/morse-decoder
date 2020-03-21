83f7ff134eb6c5d86b295755cc4545813c9a49a7
Kebikau Kiryl <kebikov1995@gmail.com>
2020-03-21 16:22:49 +0300

    feat: add functionality in task morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,9 +37,50 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode(expr){
+    let letterArray = [];
+    let letterLength = 10;
+    let i = 0;
+  
+    while(expr.length != 0){
+      letterArray[i] = expr.slice(0, letterLength);
+      expr = expr.slice(letterLength, expr.length);
+      i++;
+    }
+  
+    for(let i = 0; i < letterArray.length; i++){
+      let str = '';
+      for(let j = 0; j < letterLength - 1; j++){
+        let letterCouple = letterArray[i][j] + letterArray[i][j+1];
+        switch(letterCouple){
+          case '00':
+            str += '';
+            break;
+          case '10':
+            str += '.';
+            break;
+          case '11':
+            str += '-';
+            break;
+          case '**':
+            str += ' ';
+            break;
+          default:
+            throw Error('Input value is incorrect.');
+        }
+        j++;
+      }
+      letterArray[i] = (str == '     ') ? ' ' : str;
+    }
+  
+    for(let i = 0; i < letterArray.length; i++){
+      for(let val in MORSE_TABLE){
+        if(val == letterArray[i]) letterArray[i] = MORSE_TABLE[val];
+      }
+    }
+  
+    return letterArray.join('');
+  }
 
 module.exports = {
     decode
